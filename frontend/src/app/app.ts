deb0e85032a1b7c490af9dfb80f4cf8490248ad9
evel1noo <Evelin.Nosova@Student.HTW-Berlin.de>
2025-08-06 19:26:16 +0200

    Frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
-import { Body } from './body/body';
+import { Login } from './login/login';
 import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Body, Footer],
+  imports: [RouterOutlet, Header, Login, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- frontend/src/app/login/login.spec.ts
+++ frontend/src/app/login/login.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Body } from './body';
+import { Login } from './login';
 
-describe('Body', () => {
-  let component: Body;
-  let fixture: ComponentFixture<Body>;
+describe('Login', () => {
+  let component: Login;
+  let fixture: ComponentFixture<Login>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Body]
+      imports: [Login]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Body);
+    fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
