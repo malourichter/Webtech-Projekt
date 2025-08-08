e65eb961eb4827433a41e1751308da51e3adc7ea
Malou Richter <Malou.Richter@Student.HTW-Berelin.de>
2025-08-08 14:11:39 +0200

    Frontend weitergemacht, Verlinkung auf Registrierung angefangen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
+import { Register } from './register/register';
+import { Login } from './login/login';
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Login }, 
+    { path: 'register', component: Register }
+];
+
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Login } from './login/login';
 import { Footer } from './footer/footer';
+import { Register } from './register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Login, Footer],
+  imports: [RouterOutlet, Header, Login, Register, Footer,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
