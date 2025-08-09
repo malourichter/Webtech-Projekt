858514561d513135617e02044210fffd841bfa5b
Malou Richter <Malou.Richter@Student.HTW-Berelin.de>
2025-08-09 15:28:10 +0200

    Verlinkung zum Launentracker, Interface Laune angefangen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Register } from './register/register';
 import { Login } from './login/login';
+import { Mood } from './mood/mood';
 
 
 export const routes: Routes = [
     { path: '', component: Login }, 
-    { path: 'register', component: Register }
+    { path: 'register', component: Register },
+    { path: 'mood', component: Mood}
 ];
 
---
+++ frontend/src/app/mood/mood.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mood',
+  imports: [],
+  templateUrl: './mood.html',
+  styleUrl: './mood.css'
+})
+export class Mood {
+
+}
