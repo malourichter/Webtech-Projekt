bd65a0171967e2b959a778d39648f25bd7c39192
Malou Richter <Malou.Richter@Student.HTW-Berelin.de>
2025-08-10 15:36:18 +0200

    Bilder in images verlagert, Interface Laune weitergemacht, Seite Habits erstellt

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Register } from './register/register';
 import { Login } from './login/login';
 import { Mood } from './mood/mood';
+import { Habits } from './habits/habits';
 
 
 export const routes: Routes = [
     { path: '', component: Login }, 
     { path: 'register', component: Register },
-    { path: 'mood', component: Mood}
+    { path: 'mood', component: Mood},
+    { path: 'habits', component: Habits}
 ];
 
---
+++ frontend/src/app/habits/habits.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habits',
+  imports: [],
+  templateUrl: './habits.html',
+  styleUrl: './habits.css'
+})
+export class Habits {
+
+}
