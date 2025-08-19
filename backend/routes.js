ae03b3d4347069dbb252dfbc9efb5b60b0b74ee9
evel1noo <Evelin.Nosova@Student.HTW-Berlin.de>
2025-08-19 14:00:15 +0200

    Fehlerbehebung 3

--- backend/routes.js
+++ backend/routes.js
@@ -1,10 +1,4 @@
 const express = require('express');
-const router = express.Router();
 
-// eine GET-Anfrage
-router.get('/', async(req, res) => {
-
-    res.send({ message: "Hello FIW!" });
-});
 
 module.exports = router;
