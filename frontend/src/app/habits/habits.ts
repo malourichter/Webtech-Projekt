0cecc66dc0acff41b546aff87bb2aed408cc06e7
Malou Richter <Malou.Richter@Student.HTW-Berelin.de>
2025-09-11 21:32:03 +0200

    Habits Buttons

--- frontend/src/app/habits/habits.ts
+++ frontend/src/app/habits/habits.ts
@@ -1,12 +1,25 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-habits',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './habits.html',
   styleUrl: './habits.css'
 })
 export class Habits {
+activeEmojis = new Set<string>();
 
+toggleActive(emoji: string) {
+  if (this.activeEmojis.has(emoji)) {
+    this.activeEmojis.delete(emoji);
+  } else {
+    this.activeEmojis.add(emoji);
+  }
+}
+
+isActive(emoji: string): boolean {
+  return this.activeEmojis.has(emoji);
+}
 }
