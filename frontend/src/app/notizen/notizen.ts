f9528380f0826485d5f25d4444b054f6ab61e4f2
evel1noo <Evelin.Nosova@Student.HTW-Berlin.de>
2025-09-15 14:28:04 +0200

    Neue Fehlerbehebung

--- frontend/src/app/notizen/notizen.ts
+++ frontend/src/app/notizen/notizen.ts
@@ -11,41 +11,42 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class Notizen {
   notiz: string = '';
+  error: string = '';
 
   constructor(private router: Router, private http: HttpClient) {}
 
   saveEntry() {
-    const userId = localStorage.getItem('userId') || '';
-    const mood = { name: localStorage.getItem('mood') || '' };
-    const habits = JSON.parse(localStorage.getItem('habits') || '[]');
-    const datum = new Date();
-    const uhrzeit = new Date().toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' });
+  const userId = localStorage.getItem('userId') || '';
+  const mood = { name: localStorage.getItem('mood') || '' };
+  const habits = JSON.parse(localStorage.getItem('habits') || '[]');
+  const datum = new Date();
+  const uhrzeit = new Date().toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' });
 
-    const token = localStorage.getItem('token');
-    const headers = new HttpHeaders()
-      .set('Content-Type', 'application/json')
-      .set('Authorization', `Bearer ${token}`);
+  const token = localStorage.getItem('token');
+  const headers = new HttpHeaders()
+    .set('Content-Type', 'application/json')
+    .set('Authorization', `Bearer ${token}`);
 
-    this.http.post<any>('http://localhost:3000/entry', {
-      userId,
-      mood,
-      habits,
-      notizen: this.notiz,
-      datum,
-      uhrzeit
-    }, { headers }).subscribe({
-      next: () => {
-        localStorage.removeItem('mood');
-        localStorage.removeItem('habits');
-        localStorage.removeItem('notizen');
-        this.router.navigate(['/eintraege']);
-      },
-      error: () => {
-        
-      }
-    });
-  }
-}
+  localStorage.setItem('notizen', this.notiz);
 
-
-// Test
+  this.http.post<any>('http://localhost:3000/entry', {
+    userId,
+    mood,
+    habits,
+    notizen: this.notiz,
+    datum,
+    uhrzeit
+  }, { headers }).subscribe({
+    next: () => {
+      localStorage.removeItem('mood');
+      localStorage.removeItem('habits');
+      localStorage.removeItem('notizen');
+      
+      this.router.navigate(['/eintraege']);
+    },
+    error: (err) => {
+      this.error = err.error?.message || 'Speichern fehlgeschlagen!';
+     
+    }
+  });
+}}
